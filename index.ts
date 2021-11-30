7deb7889e1fded06e362e138735ac76b178bee0e
Matheus Henrique Da Silva <matheushenriquedasilvaa.2021@gmail.com>
2021-11-30 17:13:20 -0300

    typescript na pratica

--- index.ts
+++ index.ts
@@ -1,37 +1,71 @@
-const somar = (a: number, b: number) => a + b;
-console.log(somar(5, 5));
+// inputs
+const input = document.getElementById('input') as HTMLInputElement;
+input.addEventListener('input', event => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value)
+});
 
-// interface - Contrato para implementar suas classes 
+// generic types
+function adicionaApendiceALista<NaoSei>(array: any[], valor: NaoSei) {
+   return array.map(item => item + valor);
+};
 
-interface Animal {
+adicionaApendiceALista([1, 2, 3], 1);
+
+// --------------------------------------
+
+interface Usuario {
+    id: string;
+    email: string;
+    cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
+}
+
+function redirecione(usuario: Usuario){
+    if(usuario.cargo){
+        // redirecionar(usuario.cargo);
+    }else{
+        // redirecionar para a área do usuário
+    }
+}
+
+// readonly and private
+interface AnimalDomestico {
+    readonly nome: string;
+    readonly idade: number;
+    readonly parqueFavorito?: string;
+}
+
+type CachorroSomenteLeitura = {
+    +readonly [K in keyof AnimalDomestico]-?: AnimalDomestico[K]
+}
+
+class MeuCachorro implements AnimalDomestico {
+     idade;
+     nome;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const cao = new MeuCachorro('Apolo', 14);
+
+
+// Omit
+// https://www.typescriptlang.org/docs/handbook/utility-types.html
+interface Pessoa {
     nome: string;
-    tipo: 'terrestre' | 'aquático';
-    domestico: boolean;
+    idade: number;
+    nacionalidade: string;
 }
 
-interface Felino extends Animal {
-    visaoNorturna: boolean;
+interface Brasileiro extends Omit<Pessoa, 'nacionalidade'> {
+    UF: string;
 }
 
-interface Canino extends Animal {
-    porte: 'pequeno' | 'medio' | 'grande'
-}
-
-// types - Validação de tipos 
-
-type Domestico = Felino | Canino;
-
-
-const felino: Domestico = {
-    nome: 'Leão',
-    tipo: 'terrestre',
-    visaoNorturna: true,
-    domestico: false
-}
-
-const animal: Domestico = {
-    nome: 'Elefante',
-    tipo: 'terrestre',
-    visaoNorturna: false,
-    domestico: false
+const brasileiro: Brasileiro = {
+    nome: 'Matheus H',
+    idade: 20,
+    UF: 'Pernambuco'
 }
