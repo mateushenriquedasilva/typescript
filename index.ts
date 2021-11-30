dc4329ba9e36f42dc13bd12ba3b598679ed157d3
Matheus Henrique Da Silva <matheushenriquedasilvaa.2021@gmail.com>
2021-11-30 14:15:27 -0300

    interface and types

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("typescript")
+const somar = (a: number, b: number) => a + b;
+console.log(somar(5, 5));
+
+// interface - Contrato para implementar suas classes 
+
+interface Animal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    domestico: boolean;
+}
+
+interface Felino extends Animal {
+    visaoNorturna: boolean;
+}
+
+interface Canino extends Animal {
+    porte: 'pequeno' | 'medio' | 'grande'
+}
+
+// types - Validação de tipos 
+
+type Domestico = Felino | Canino;
+
+
+const felino: Domestico = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNorturna: true,
+    domestico: false
+}
+
+const animal: Domestico = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    visaoNorturna: false,
+    domestico: false
+}
